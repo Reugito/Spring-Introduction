f13388e641bc82391d17008bd2906ac6b1da3f82
Reugito <raosahebdhotreedu@gmail.com>
2021-08-26 18:48:14 +0530

    added put and post request methods

--- NewHelloWorld/src/main/java/com/example/demo/controller/HelloRestController.java
+++ NewHelloWorld/src/main/java/com/example/demo/controller/HelloRestController.java
@@ -2,10 +2,15 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.User;
 
 @RestController
 @RequestMapping("/hello")
@@ -24,5 +29,15 @@ public class HelloRestController {
 	@GetMapping("/param/{name}")
 	public String sayHelloParam(@PathVariable String name) {
 		return "Hello "+ name +";";
+	}
+	
+	@PostMapping("/post")
+	public String SayHello(@RequestBody User user) {
+		return "Hello"+user.getFirstName()+" "+user.getLastName();
+	}
+	
+	@PutMapping("/put/firstName")
+	public String SayHello(@PathVariable String firstName, @RequestParam(value = "lastName")String lastName) {
+		return "Hello"+ firstName+" "+lastName;
 	}
 }
