1206d29c105c59e6884abfc002b256d799d3febf
Reugito <raosahebdhotreedu@gmail.com>
2021-08-26 16:16:26 +0530

    created GET Request Method and passed name as parameter

--- NewHelloWorld/src/main/java/com/example/demo/controller/HelloRestController.java
+++ NewHelloWorld/src/main/java/com/example/demo/controller/HelloRestController.java
@@ -1,14 +1,21 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloRestController {
 	
-	@RequestMapping(value = {"", "/", "/home"})
+	@RequestMapping(value = {"", "/n", "/home"})
 	public String sayHello() {
 		return "Hello this is Rest Request";
+	}
+	
+	@RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+	public String sayHello(@RequestParam(value = "name") String name) {
+		return "Hello "+ name +";";
 	}
 }
